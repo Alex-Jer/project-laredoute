6a68fff8e806f841d73cac2fead5d9c53ec82266
Alexandre <alexandrejeronimo2001@gmail.com>
2022-01-04 23:32:30 +0000

    camera fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
   plugins: ['tailwindcss'],
   rules: {
     'no-undef': 'off',
+    'no-param-reassign': 'off',
+    'no-new': 'off',
   },
 };
--- js/three.js
+++ js/three.js
@@ -1,21 +1,23 @@
 const canvas = document.getElementById('canvas');
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, 185 / 185, 0.1, 1000);
+// const camera = new THREE.PerspectiveCamera(45, 185 / 185, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(70, 1215 / 600, 0.01, 1000);
 const renderer = new THREE.WebGLRenderer();
+new THREE.OrbitControls(camera, renderer.domElement);
 let model;
 
 document.body.appendChild(canvas.cloneNode(true));
-renderer.setSize(185, 185);
-scene.background = new THREE.Color(0x32cd32);
+scene.background = new THREE.Color(0xf3f4f6);
 canvas.appendChild(renderer.domElement);
 
-camera.position.z = 20;
-camera.position.y = 3;
+camera.position.set(0, 0, 14);
+camera.lookAt(0, 0, 0);
+renderer.setSize(1215, 600);
+renderer.shadowMap.enabled = true;
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  model.rotateY(0.01);
 }
 
 function loadScene() {
@@ -23,27 +25,28 @@ function loadScene() {
   loader.load('/models/workBenchM.gltf', (gltf) => {
     model = gltf.scene;
     scene.add(model);
+    model.position.set(0, -3, 0);
 
-    // gltf.scene.traverse((x) => {
-    //   if (x instanceof THREE.Light) x.visible = false;
-    // });
+    gltf.scene.traverse((x) => {
+      if (x instanceof THREE.Light) x.visible = false;
+    });
 
-    // scene.traverse((objMesh) => {
-    //   if (objMesh.isMesh) {
-    //     objMesh.castShadow = true;
-    //     objMesh.receiveShadow = true;
-    //   }
-    // });
+    scene.traverse((objMesh) => {
+      if (objMesh.isMesh) {
+        objMesh.castShadow = true;
+        objMesh.receiveShadow = true;
+      }
+    });
   });
 }
 
 function addlights() {
-  const ambientLight = new THREE.AmbientLight(0x404040);
-  const pointLight1 = new THREE.PointLight('white');
+  const ambientLight = new THREE.AmbientLight('white');
+  const pointLight = new THREE.PointLight('white');
   scene.add(ambientLight);
-  // pointLight1.position.set(5, 6, 0);
-  pointLight1.castShadow = true;
-  scene.add(pointLight1);
+  pointLight.position.set(50, 50, 50);
+  pointLight.castShadow = true;
+  scene.add(pointLight);
 }
 
 loadScene();
